e40e770109419755b1e627c9d2f956974ac90f66
JoeMobarak <mobarak.joe@gmail.com>
2019-09-16 23:41:09 +0300

    Refactor main activity to repository activity

---
+++ app/src/main/java/com/example/maticcodechallenge/ui/activity/RepositoryActivity.kt
@@ -0,0 +1,13 @@
+package com.example.maticcodechallenge.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.maticcodechallenge.R
+
+class RepositoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repository)
+    }
+}
