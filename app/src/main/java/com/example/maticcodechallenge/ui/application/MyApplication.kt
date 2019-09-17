5e1fdd2472c334985c0a713ab7c8aba8e86530ea
JoeMobarak <mobarak.joe@gmail.com>
2019-09-17 21:15:00 +0300

    Inject data source with api class

--- app/src/main/java/com/example/maticcodechallenge/ui/application/MyApplication.kt
+++ app/src/main/java/com/example/maticcodechallenge/ui/application/MyApplication.kt
@@ -1,15 +1,22 @@
 package com.example.maticcodechallenge.ui.application
 
 import android.app.Application
+import data.source.RepositoryDataSource
+import network.api.RepositoryAPI
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
 
 class MyApplication : Application(), KodeinAware {
 
     //Setup Dependency Injection
     override val kodein = Kodein.lazy {
         import(androidXModule(this@MyApplication))
+        bind() from provider { RepositoryDataSource(instance()) }
+        bind() from provider { RepositoryAPI() }
     }
 
 }
