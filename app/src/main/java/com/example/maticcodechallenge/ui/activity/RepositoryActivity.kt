cac11dac6ed20149c4e574919cc082aca4eaf390
JoeMobarak <mobarak.joe@gmail.com>
2019-09-17 21:32:30 +0300

    Lateinit the needed variables in activity and make it kodeinaware

--- app/src/main/java/com/example/maticcodechallenge/ui/activity/RepositoryActivity.kt
+++ app/src/main/java/com/example/maticcodechallenge/ui/activity/RepositoryActivity.kt
@@ -2,12 +2,28 @@ package com.example.maticcodechallenge.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.maticcodechallenge.R
+import com.example.maticcodechallenge.databinding.RepositoryActivityBinding
+import com.example.maticcodechallenge.ui.adapter.RepositoriesAdapter
+import com.example.maticcodechallenge.ui.viewmodel.RepositoryViewModel
+import com.example.maticcodechallenge.ui.viewmodel.RepositoryViewModelFactory
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.closestKodein
+import org.kodein.di.generic.instance
 
-class RepositoryActivity : AppCompatActivity() {
+class RepositoryActivity : AppCompatActivity(), KodeinAware {
+
+    private lateinit var adapter: RepositoriesAdapter
+    private lateinit var viewModel: RepositoryViewModel
+    private lateinit var binding: RepositoryActivityBinding
+    override val kodein by closestKodein()
+    private val viewModelFactory: RepositoryViewModelFactory by instance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_repository)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_repository)
+
     }
+    
 }
